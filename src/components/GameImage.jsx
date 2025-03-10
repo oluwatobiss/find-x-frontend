7220d2bce91a0e42dadbc652bed3537b421efc8d
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-03-10 19:43:58 +0100

    feat: Specify items to find

--- src/components/GameImage.jsx
+++ src/components/GameImage.jsx
@@ -1,4 +1,4 @@
-import donutPile from "../assets/rory-adams-blueberrybamf-donut-pile-colored.jpg";
+import donutPile from "../assets/images/donut-pile.jpg";
 
 export default function GameImage() {
   return <img src={donutPile.src} alt="Donut pile" width={"100%"} />;
---
+++ src/itemsToFind.js
@@ -0,0 +1,10 @@
+const itemsToFind = [
+  { name: "American football", fileName: "american-football.png" },
+  { name: "Carrot", fileName: "carrot.png" },
+  { name: "Hamburger", fileName: "hamburger.png" },
+  { name: "Pack of fries", fileName: "french-fries.png" },
+  { name: "Pineapple", fileName: "pineapple.png" },
+  { name: "Pizza", fileName: "pizza.png" },
+];
+
+export default itemsToFind;
