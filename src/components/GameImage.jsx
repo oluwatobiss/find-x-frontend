9726528de64981a9718753d6db4eb0419693b36e
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-03-10 18:47:39 +0100

    feat: Create GameImage component

---
+++ src/components/GameImage.jsx
@@ -0,0 +1,5 @@
+import donutPile from "../assets/rory-adams-blueberrybamf-donut-pile-colored.jpg";
+
+export default function GameImage() {
+  return <img src={donutPile.src} alt="Donut pile" width={"100%"} />;
+}
