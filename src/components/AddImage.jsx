ffe5d083e04233a18e5e4ec0456ccdb8dc20c64d
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-03-19 15:25:11 +0100

    style: Design form

--- src/components/AddImage.jsx
+++ src/components/AddImage.jsx
@@ -67,12 +67,13 @@ export default function LoginForm() {
 
   const itemFieldsets = itemsData.map((data, index) => (
     <section key={data.id}>
-      <fieldset>
-        <legend>Item {index + 1}</legend>
-        <label>
+      <fieldset className="my-8 border border-gray-500 rounded-sm px-10 py-7">
+        <legend className="px-2 uppercase font-semibold">
+          Item {index + 1}
+        </legend>
+        <label className="w-full">
           Name
           <input
-            className="text-input"
             type="text"
             name="name"
             value={data.name}
@@ -80,92 +81,93 @@ export default function LoginForm() {
             required
           />
         </label>
-        <div>Item {index + 1}'s pixel location</div>
-        <label>
-          Center-X (px)
-          <input
-            className="text-input"
-            type="number"
-            name="centerX"
-            value={data.centerX}
-            onChange={(e) => updateItemsData(e, index)}
-            required
-          />
-        </label>
-        <label>
-          Center-Y (px)
-          <input
-            className="text-input"
-            type="number"
-            name="centerY"
-            value={data.centerY}
-            onChange={(e) => updateItemsData(e, index)}
-            required
-          />
-        </label>
-        <label>
-          Start-X (px)
-          <input
-            className="text-input"
-            type="number"
-            name="startX"
-            value={data.startX}
-            onChange={(e) => updateItemsData(e, index)}
-            required
-          />
-        </label>
-        <label>
-          Start-Y (px)
-          <input
-            className="text-input"
-            type="number"
-            name="startY"
-            value={data.startY}
-            onChange={(e) => updateItemsData(e, index)}
-            required
-          />
-        </label>
-        <label>
-          End-X (px)
-          <input
-            className="text-input"
-            type="number"
-            name="endX"
-            value={data.endX}
-            onChange={(e) => updateItemsData(e, index)}
-            required
-          />
-        </label>
-        <label>
-          End-Y (px)
-          <input
-            className="text-input"
-            type="number"
-            name="endY"
-            value={data.endY}
-            onChange={(e) => updateItemsData(e, index)}
-            required
-          />
-        </label>
+        <div className="pt-5 text-sm uppercase font-semibold">
+          Pixel location
+        </div>
+        <div className="flex gap-x-3">
+          <label>
+            Center-X (px)
+            <input
+              className="text-input"
+              type="number"
+              name="centerX"
+              value={data.centerX}
+              onChange={(e) => updateItemsData(e, index)}
+              required
+            />
+          </label>
+          <label>
+            Center-Y (px)
+            <input
+              className="text-input"
+              type="number"
+              name="centerY"
+              value={data.centerY}
+              onChange={(e) => updateItemsData(e, index)}
+              required
+            />
+          </label>
+        </div>
+        <div className="flex gap-x-3">
+          <label>
+            Start-X (px)
+            <input
+              className="text-input"
+              type="number"
+              name="startX"
+              value={data.startX}
+              onChange={(e) => updateItemsData(e, index)}
+              required
+            />
+          </label>
+          <label>
+            Start-Y (px)
+            <input
+              className="text-input"
+              type="number"
+              name="startY"
+              value={data.startY}
+              onChange={(e) => updateItemsData(e, index)}
+              required
+            />
+          </label>
+        </div>
+        <div className="flex gap-x-3">
+          <label>
+            End-X (px)
+            <input
+              className="text-input"
+              type="number"
+              name="endX"
+              value={data.endX}
+              onChange={(e) => updateItemsData(e, index)}
+              required
+            />
+          </label>
+          <label>
+            End-Y (px)
+            <input
+              className="text-input"
+              type="number"
+              name="endY"
+              value={data.endY}
+              onChange={(e) => updateItemsData(e, index)}
+              required
+            />
+          </label>
+        </div>
+        {itemsData.length > 1 && (
+          <button
+            type="button"
+            onClick={() =>
+              setItemsData(itemsData.filter((item) => item.id !== data.id))
+            }
+            className="cursor-pointer bg-gray-200 mt-3 px-7 py-2 border border-gray-400 border-solid rounded-sm"
+          >
+            Delete {data.name}
+          </button>
+        )}
       </fieldset>
-      {itemsData.length > 1 && (
-        <button
-          type="button"
-          onClick={() =>
-            setItemsData(itemsData.filter((item) => item.id !== data.id))
-          }
-          className="cursor-pointer bg-gray-200 mt-3 px-7 py-2 border border-gray-400 border-solid rounded-sm"
-        >
-          Remove {data.name ? data.name : "item"}
-        </button>
-      )}
-      <button
-        type="button"
-        onClick={() => setItemsData([...itemsData, createItemData()])}
-        className="cursor-pointer bg-gray-200 mt-3 px-7 py-2 border border-gray-400 border-solid rounded-sm"
-      >
-        Add new item
-      </button>
     </section>
   ));
 
@@ -198,13 +200,23 @@ export default function LoginForm() {
           />
           {/* {showErrorFor("url")} */}
         </div>
+        <h2>Specify items to find</h2>
         {itemFieldsets}
-        <button
-          type="submit"
-          className="cursor-pointer bg-gray-200 mt-3 px-7 py-2 border border-gray-400 border-solid rounded-sm"
-        >
-          Add image
-        </button>
+        <section className="flex gap-x-3">
+          <button
+            type="button"
+            onClick={() => setItemsData([...itemsData, createItemData()])}
+            className="cursor-pointer bg-gray-200 mt-3 px-7 py-2 border border-gray-400 border-solid rounded-sm"
+          >
+            Add new item
+          </button>
+          <button
+            type="submit"
+            className="cursor-pointer bg-gray-200 mt-3 px-7 py-2 border border-gray-400 border-solid rounded-sm"
+          >
+            Submit image
+          </button>
+        </section>
       </form>
     </>
   );
