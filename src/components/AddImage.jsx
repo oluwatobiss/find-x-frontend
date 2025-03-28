2b5ef36d22f9f73462b434b5142ddcf8005b380d
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-03-28 21:54:49 +0100

    feat: Create logic to submit new images to db

--- src/components/AddImage.jsx
+++ src/components/AddImage.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 
 export default function AddImage() {
   const [imageName, setImageName] = useState("");
-  const [url, setUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
   const [itemsData, setItemsData] = useState([createItemData()]);
   const [errors, setErrors] = useState([]);
 
   function createItemData() {
     return {
       id: crypto.randomUUID(),
-      name: "",
+      itemName: "",
+      itemImageUrl: "",
       centerX: "",
       centerY: "",
       startX: "",
@@ -19,45 +20,43 @@ export default function AddImage() {
     };
   }
 
-  // async function authenticateUser(e) {
-  //   e.preventDefault();
-  //   try {
-  //     const response = await fetch(
-  //       `${import.meta.env.PUBLIC_BACKEND_URI}/auths`,
-  //       {
-  //         method: "POST",
-  //         body: JSON.stringify({ email, password }),
-  //         headers: { "Content-type": "application/json; charset=UTF-8" },
-  //       }
-  //     );
-  //     const userData = await response.json();
-  //     localStorage.setItem("apiPoweredBlogToken", userData.token);
-  //     localStorage.setItem(
-  //       "apiPoweredBlogUserData",
-  //       JSON.stringify(userData.payload)
-  //     );
-  //     if (iframeUseRef.current) {
-  //       const targetOrigin = fansEndUri;
-  //       const iframeWindow = iframeUseRef.current.contentWindow;
-  //       iframeWindow.postMessage(userData, targetOrigin);
-  //     }
-  //     userData.errors?.length
-  //       ? setErrors(userData.errors)
-  //       : (window.location.href = "/");
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       console.error(error.message);
-  //     }
-  //   }
-  // }
+  async function submitImageData(e) {
+    e.preventDefault();
 
-  // function showErrorFor(field) {
-  //   return errors.find((c) => c.path === field) ? (
-  //     <div className="error">{errors.find((c) => c.path === field).msg}</div>
-  //   ) : (
-  //     ""
-  //   );
-  // }
+    console.log({ imageName, imageUrl, itemsData });
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.PUBLIC_BACKEND_URI}/images`,
+        {
+          method: "POST",
+          body: JSON.stringify({ imageName, imageUrl, itemsData }),
+          headers: { "Content-type": "application/json; charset=UTF-8" },
+        }
+      );
+      const imageData = await response.json();
+      console.log("=== submitImageData in AddImage component ===");
+      console.log(imageData);
+
+      imageData.errors?.length
+        ? setErrors(imageData.errors)
+        : (window.location.href = "/");
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  function showErrorFor(field) {
+    return errors.find((error) => error.path === field) ? (
+      <div className="mb-2 text-sm text-red-500">
+        {errors.find((error) => error.path === field).msg}
+      </div>
+    ) : (
+      ""
+    );
+  }
 
   function updateItemsData(event, index) {
     const itemsData_ = [...itemsData];
@@ -66,7 +65,7 @@ export default function AddImage() {
   }
 
   const itemFieldsets = itemsData.map((data, index) => (
-    <section key={data.id}>
+    <section key={data.id} id={data.id}>
       <fieldset className="my-8 border border-gray-500 rounded-sm px-10 py-7">
         <legend className="px-2 uppercase font-semibold">
           Item {index + 1}
@@ -75,20 +74,26 @@ export default function AddImage() {
           Name
           <input
             type="text"
-            name="name"
-            value={data.name}
+            name="itemName"
+            value={data.itemName}
             onChange={(e) => updateItemsData(e, index)}
             required
           />
+          {errors.find(
+            (error) => error.path === `itemsData[${index}].itemName`
+          ) && showErrorFor(`itemsData[${index}].itemName`)}
         </label>
         <label className="w-full">
           Item image's URL
           <input
             type="url"
             name="itemImageUrl"
-            value={data.name}
+            value={data.itemImageUrl}
             onChange={(e) => updateItemsData(e, index)}
           />
+          {errors.find(
+            (error) => error.path === `itemsData[${index}].itemImageUrl`
+          ) && showErrorFor(`itemsData[${index}].itemImageUrl`)}
         </label>
         <div className="pt-5 text-sm uppercase font-semibold">
           Pixel location
@@ -104,6 +109,9 @@ export default function AddImage() {
               onChange={(e) => updateItemsData(e, index)}
               required
             />
+            {errors.find(
+              (error) => error.path === `itemsData[${index}].centerX`
+            ) && showErrorFor(`itemsData[${index}].centerX`)}
           </label>
           <label>
             Center-Y (px)
@@ -115,6 +123,9 @@ export default function AddImage() {
               onChange={(e) => updateItemsData(e, index)}
               required
             />
+            {errors.find(
+              (error) => error.path === `itemsData[${index}].centerY`
+            ) && showErrorFor(`itemsData[${index}].centerY`)}
           </label>
         </div>
         <div className="flex gap-x-3">
@@ -128,6 +139,9 @@ export default function AddImage() {
               onChange={(e) => updateItemsData(e, index)}
               required
             />
+            {errors.find(
+              (error) => error.path === `itemsData[${index}].startX`
+            ) && showErrorFor(`itemsData[${index}].startX`)}
           </label>
           <label>
             Start-Y (px)
@@ -139,6 +153,9 @@ export default function AddImage() {
               onChange={(e) => updateItemsData(e, index)}
               required
             />
+            {errors.find(
+              (error) => error.path === `itemsData[${index}].startY`
+            ) && showErrorFor(`itemsData[${index}].startY`)}
           </label>
         </div>
         <div className="flex gap-x-3">
@@ -152,6 +169,9 @@ export default function AddImage() {
               onChange={(e) => updateItemsData(e, index)}
               required
             />
+            {errors.find(
+              (error) => error.path === `itemsData[${index}].endX`
+            ) && showErrorFor(`itemsData[${index}].endX`)}
           </label>
           <label>
             End-Y (px)
@@ -163,6 +183,9 @@ export default function AddImage() {
               onChange={(e) => updateItemsData(e, index)}
               required
             />
+            {errors.find(
+              (error) => error.path === `itemsData[${index}].endY`
+            ) && showErrorFor(`itemsData[${index}].endY`)}
           </label>
         </div>
         {itemsData.length > 1 && (
@@ -182,32 +205,36 @@ export default function AddImage() {
 
   return (
     <>
-      {/* <form onSubmit={authenticateUser}> */}
-      <form className="[&_input]:w-full [&_input]:border [&_input]:border-gray-500 [&_input]:rounded-sm [&_input]:my-1 [&_input]:px-5 [&_input]:py-2 [&_input]:text-lg [&_label]:inline-block [&_label]:text-sm [&_label]:mt-3">
+      <form
+        className="[&_input]:w-full [&_input]:border [&_input]:border-gray-500 [&_input]:rounded-sm [&_input]:my-1 [&_input]:px-5 [&_input]:py-2 [&_input]:text-lg [&_label]:inline-block [&_label]:text-sm [&_label]:mt-3"
+        onSubmit={submitImageData}
+      >
         <div>
-          <label htmlFor="name">Name</label>
+          <label htmlFor="imageName">Name</label>
           <input
             className="text-input"
             type="text"
-            name="name"
-            id="name"
+            name="imageName"
+            id="imageName"
             value={imageName}
             onChange={(e) => setImageName(e.target.value)}
             required
           />
-          {/* {showErrorFor("imageName")} */}
+          {errors.find((error) => error.path === "imageName") &&
+            showErrorFor("imageName")}
         </div>
         <div>
-          <label htmlFor="url">URL</label>
+          <label htmlFor="imageUrl">URL</label>
           <input
             type="url"
-            name="url"
-            id="url"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
+            name="imageUrl"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
             required
           />
-          {/* {showErrorFor("url")} */}
+          {errors.find((error) => error.path === "imageUrl") &&
+            showErrorFor("imageUrl")}
         </div>
         <h2>Specify items to find</h2>
         {itemFieldsets}
