f2a57c7b08ee8ed1033cc2ce51c766e139adb8e4
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-03-31 13:26:31 +0100

    feat: Create nav bar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export default function Navigation() {
+  const [userToken, setUserToken] = useState("");
+  const [userStatus, setUserStatus] = useState("");
+
+  function logoutUser() {
+    localStorage.removeItem("findXToken");
+    localStorage.removeItem("findXUserData");
+    window.location.href = "/";
+  }
+
+  useEffect(() => {
+    let userToken = localStorage.getItem("findXToken");
+    let userDataJson = localStorage.getItem("findXUserData");
+
+    userToken === "undefined" && (userToken = undefined);
+    userDataJson === "undefined" && (userDataJson = undefined);
+
+    const userData = userDataJson && JSON.parse(userDataJson);
+    userToken && setUserToken(userToken);
+    userData?.status && setUserStatus(userData.status);
+  }, []);
+
+  return (
+    <>
+      <div className="text-center [&_a,&_button]:px-4 [&_a,&_button]:py-5 [&_a,&_button]:text-xl [&_a,&_button]:font-bold [&_a,&_button]:uppercase [&_a,&_button]:text-blue-700 [&_a,&_button]:hover:bg-orange-300 [&_a,&_button]:focus:bg-orange-300 [&_button]:cursor-pointer">
+        <a href="/">Find-X</a>
+        {!userStatus && <a href="/sign-up">Sign up</a>}
+        {!userToken && <a href="/log-in">Log in</a>}
+        {userToken && userStatus === "ADMIN" && (
+          <a href="/dashboard/">Dashboard</a>
+        )}
+        {userToken && (
+          <>
+            <a href="/leaderboard/">Leaderboard</a>
+            <button type="button" onClick={logoutUser}>
+              Log out
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
