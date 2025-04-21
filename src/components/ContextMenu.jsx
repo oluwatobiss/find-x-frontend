d60a60e27d64f40c290a1a2a8528cd9093f1fbc4
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-04-21 12:33:13 +0100

    fix: Check if an item is found after updating clickedMenu state
    This needs useEffect hook because onClick's event handler (recordClickedMenu) is a DOM function
    
    recordClickedMenu's location matters because the hook updating the clickedMenu state resides in it

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import markItem from "../markItem";
 
 export default function ContextMenu({
@@ -10,56 +10,76 @@ export default function ContextMenu({
   const menuItemsSet = useRef(false);
   const menuClicked = useRef(false);
   const [menuItems, setMenuItems] = useState([]);
-  const [clickedMenu, setClickedMenu] = useState("");
+  const [clickedMenu, setClickedMenu] = useState({
+    forceUpdateClickedMenu: true,
+    itemName: "",
+  });
 
   function recordClickedMenu(e) {
+    console.log("=== start recordClickedMenu start ===");
+    const clickedItemName = e.currentTarget.getAttribute("data-menu-item-name");
     menuClicked.current = true;
-    setClickedMenu(e.currentTarget.getAttribute("data-menu-item-name"));
+
+    console.log(clickedMenu.itemName);
+    console.log(clickedItemName);
+    console.log(clickedMenu.forceUpdateClickedMenu);
+
+    setClickedMenu({
+      forceUpdateClickedMenu: !clickedMenu.forceUpdateClickedMenu,
+      itemName: clickedItemName,
+    });
     contextMenuRef.current.classList.remove("visible");
     contextMenuRef.current.classList.add("invisible");
     targetingBoxRef.current.classList.remove("visible");
     targetingBoxRef.current.classList.add("invisible");
+    console.log("=== end recordClickedMenu end ===");
   }
 
-  if (menuClicked.current) {
-    console.log("=== ContextMenu ===");
-    console.log(clickedSpotEvent);
-    console.log(imageItems);
-    console.log({ clickedMenu });
+  useEffect(() => {
+    if (menuClicked.current) {
+      console.log("=== ContextMenu ===");
+      console.log(clickedSpotEvent);
+      console.log(imageItems);
+      console.log(clickedMenu);
 
-    // imageRect = Info about the image's size and position relative to the browser's viewport
-    // clientX = x-position of the mouse click relative to the browser's viewport
-    // clientY = y-position of the mouse click relative to the browser's viewport
-    // clientX/Y illustration --> https://stackoverflow.com/a/21452887/11841906
-    // imageClientX = clientX minus any offset on the image's left
-    // imageClientY = clientY minus any offset on the image's top
-    // itemX = center-x-position of the item relative to the image
-    // itemY = center-y-position of the item relative to the image
+      // imageRect = Info about the image's size and position relative to the browser's viewport
+      // clientX = x-position of the mouse click relative to the browser's viewport
+      // clientY = y-position of the mouse click relative to the browser's viewport
+      // clientX/Y illustration --> https://stackoverflow.com/a/21452887/11841906
+      // imageClientX = clientX minus any offset on the image's left
+      // imageClientY = clientY minus any offset on the image's top
+      // itemX = center-x-position of the item relative to the image
+      // itemY = center-y-position of the item relative to the image
 
-    const imageRect = clickedSpotEvent.target.getBoundingClientRect();
-    const imageClientX = clickedSpotEvent.clientX - imageRect.left;
-    const imageClientY = clickedSpotEvent.clientY - imageRect.top;
-    const menuItem = imageItems.find((item) => item.itemName === clickedMenu);
+      const imageRect = clickedSpotEvent.target.getBoundingClientRect();
+      const imageClientX = clickedSpotEvent.clientX - imageRect.left;
+      const imageClientY = clickedSpotEvent.clientY - imageRect.top;
+      const menuItem = imageItems.find(
+        (item) => item.itemName === clickedMenu.itemName
+      );
 
-    console.log(imageRect);
-    console.log(menuItem);
+      console.log(imageRect);
+      console.log(menuItem);
 
-    if (
-      imageClientX >= menuItem?.startX * imageRect.width &&
-      imageClientX <= menuItem?.endX * imageRect.width &&
-      imageClientY >= menuItem?.startY * imageRect.height &&
-      imageClientY <= menuItem?.endY * imageRect.height
-    ) {
-      console.log("=== You found an item! ===");
-      console.log(`${clickedMenu} found!`);
+      if (
+        imageClientX >= menuItem?.startX * imageRect.width &&
+        imageClientX <= menuItem?.endX * imageRect.width &&
+        imageClientY >= menuItem?.startY * imageRect.height &&
+        imageClientY <= menuItem?.endY * imageRect.height
+      ) {
+        console.log("=== You found an item! ===");
+        console.log(`${clickedMenu.itemName} found!`);
 
-      const itemX = (menuItem.centerX * imageRect.width) + imageRect.left; // prettier-ignore
-      const itemY = (menuItem.centerY * imageRect.height) + imageRect.top; // prettier-ignore
+        const itemX = (menuItem.centerX * imageRect.width) + imageRect.left; // prettier-ignore
+        const itemY = (menuItem.centerY * imageRect.height) + imageRect.top; // prettier-ignore
 
-      markItem({ itemX, itemY, itemName: menuItem.itemName });
+        markItem({ itemX, itemY, itemName: menuItem.itemName });
+      }
+      menuClicked.current = false;
+      console.log("=== Last ===");
+      console.log(menuClicked);
     }
-    menuClicked.current = false;
-  }
+  }, [clickedMenu]);
 
   if (imageItems && !menuItemsSet.current) {
     const menuItems = imageItems.map((item) => {
